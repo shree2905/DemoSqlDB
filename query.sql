4ebf85d6f57e204e50c177c050b7cd35f2282888
shree2905 <79618656+shree2905@users.noreply.github.com>
2021-03-02 16:15:35 +0530

    Add files via upload

---
+++ query.sql
@@ -0,0 +1,13 @@
+create table tutorials_tbl(
+   tutorial_id INT NOT NULL,
+   tutorial_title VARCHAR(100) NOT NULL,
+   tutorial_author VARCHAR(40) NOT NULL,
+   submission_date DATE,
+   PRIMARY KEY ( tutorial_id )
+);
+
+insert into [dbo].[tutorials_tbl] values (1,'php', 'harry', '06-01-2019')
+insert into [dbo].[tutorials_tbl] values (2,'c#', 'jon', '02-16-2020')
+insert into [dbo].[tutorials_tbl] values (3,'PowerShell', 'monica', '01-26-2019')
+insert into [dbo].[tutorials_tbl] values (4,'python', 'dev', '06-23-2019')
+insert into [dbo].[tutorials_tbl] values (5,'pearl', 'janice', '01-17-2021')
